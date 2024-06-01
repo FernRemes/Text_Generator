ab79aee33d26bc572dfe8e8607ad71b11ecca80c
Fernando Remes <58086702+FernRemes@users.noreply.github.com>
2024-06-01 12:44:49 -0600

    adding Numpy

--- main.py
+++ main.py
@@ -20,3 +20,23 @@ char_to_index = dict((c, i) for i, c in enumerate(characters))
 index_to_char = dict((i, c) for i, c in enumerate(characters))
 
 
+SEQ_LENGTH = 40
+STEP_SIZE = 3
+
+sentences = []
+next_chars = []
+
+for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
+    sentences.append(text[i: i + SEQ_LENGTH])
+    next_chars.append(text[i + SEQ_LENGTH])
+
+x = np.zeros(len(sentences), SEQ_LENGTH, len(characters), dtype = bool)
+y = np.zeros(len(sentences), len(characters), dtype = bool)
+
+for i, sentence in enumerate(sentences):
+    for t, character in enumerate(sentence):
+        x[i,t, char_to_index[character]] = 1
+    y[i, char_to_index[next_chars[i]]] = 1
+
+
+
