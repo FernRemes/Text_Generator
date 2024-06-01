2144bddb0da69c8b1fa21b01f93670cdbb6fbe84
Fernando Remes <58086702+FernRemes@users.noreply.github.com>
2024-06-01 11:45:49 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Activation
+from tensorflow.keras.optimizers import RMSprop
+
+
